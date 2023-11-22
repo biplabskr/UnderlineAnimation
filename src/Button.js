2c13fea7ef37861c12df6fe13af1c6e633f5fd22
Biplab Sarkar <biplabskr24@gmail.com>
2023-11-22 15:40:37 +0530

    underlineanimation

---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Button.css"
+
+export default function Button() {
+  return (
+    <div>
+        <a>
+            <span className='span1'></span>
+            <span className='span2'></span>
+            btn
+        </a>
+    </div>
+  )
+}
+
+
+
+
